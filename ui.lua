f13678f1ea82d4c09e0f263abcaba56032acf89c
3itx <100333941+Looser3itx@users.noreply.github.com>
2024-02-20 03:04:25 -0500

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -19,7 +19,7 @@ function Library:CreateUI(uiname,scrollenabled,elementspacing,framesize,TextAlli
     MainFrame.BorderColor3 = Color3.fromRGB(25, 25, 25)
     MainFrame.BorderSizePixel = 0
     MainFrame.Position = UDim2.new(0.346445292, 0, 0.247718394, 0)
-    MainFrame.Size = UDim2.new(0, 350, 0, 340)
+    MainFrame.Size = UDim2.new(0, 350, 0, 360)
     
     Dashboard.Name = "Dashboard"
     Dashboard.Parent = MainFrame
